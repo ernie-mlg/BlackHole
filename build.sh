aeae1145b83566f649d61080f7e010e7c00a088c
yossi-tahara <yossi-tahara@theolizer.com>
2022-03-02 12:57:22 +0900

    テンプレート化とビルド・スクリプト実装

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$1" = "" ] || [ "$2" = "" ] || [ "$3" = "" ];then
+    echo "illigal parameter"
+    exit 1
+fi
+
+ASSEMBLY_NAME=$1
+UUID=$2
+DERIVEDDATAPATH=$3
+
+cat ./BlackHole/BlackHole.h.template                                            | envsubst > ./BlackHole/BlackHole.h
+cat ./BlackHole/BlackHole-Info.plist.template                                   | envsubst > ./BlackHole/BlackHole-Info.plist
+cat ./BlackHole.xcodeproj/project.pbxproj.template                              | envsubst > ./BlackHole.xcodeproj/project.pbxproj
+cat ./BlackHole.xcodeproj/xcshareddata/xcschemes/BlackHole.xcscheme.template    | envsubst > ./BlackHole.xcodeproj/xcshareddata/xcschemes/BlackHole.xcscheme
+
+xcodebuild -project ./BlackHole.xcodeproj -scheme BlackHole -configuration Release -derivedDataPath $DERIVEDDATAPATH
