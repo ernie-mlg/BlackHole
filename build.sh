fe6857483b7bddc84de0e99a85a2a00ccc30a101
yossi-tahara <yossi-tahara@theilizer.com>
2024-04-09 17:46:27 +0900

    ASSEMBLY_NAMEをAPP_NAMEへ変数名変更

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ if [ "$1" = "" ] || [ "$2" = "" ] || [ "$3" = "" ] || [ "$4" = "" ];then
     exit 1
 fi
 
-ASSEMBLY_NAME=$1
+APP_NAME=$1
 APP_DEVELOPER=$2
 UUID=$3
 DERIVEDDATAPATH=$4
@@ -14,5 +14,5 @@ cat ./BlackHole/BlackHole-Info.plist.template                                   
 cat ./BlackHole.xcodeproj/project.pbxproj.template                              | envsubst > ./BlackHole.xcodeproj/project.pbxproj
 cat ./BlackHole.xcodeproj/xcshareddata/xcschemes/BlackHole.xcscheme.template    | envsubst > ./BlackHole.xcodeproj/xcshareddata/xcschemes/BlackHole.xcscheme
 
-xcodebuild -project ./BlackHole.xcodeproj -scheme BlackHole -configuration Release -derivedDataPath $DERIVEDDATAPATH GCC_PREPROCESSOR_DEFINITIONS='$GCC_PREPROCESSOR_DEFINITIONS kDriver_Name=\"'${ASSEMBLY_NAME}_SpeakerCapture'\" kHas_Driver_Name_Format=false kManufacturer_Name=\"'${APP_DEVELOPER}'\"' -destination 'platform=macOS,arch=x86_64'
+xcodebuild -project ./BlackHole.xcodeproj -scheme BlackHole -configuration Release -derivedDataPath $DERIVEDDATAPATH GCC_PREPROCESSOR_DEFINITIONS='$GCC_PREPROCESSOR_DEFINITIONS kDriver_Name=\"'${APP_NAME}_SpeakerCapture'\" kHas_Driver_Name_Format=false kManufacturer_Name=\"'${APP_DEVELOPER}'\"' -destination 'platform=macOS,arch=x86_64'
 
